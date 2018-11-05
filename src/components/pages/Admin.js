c5987ae8a21a680d296fdc1599f019946f8e638f
Omer Dogan <omerdogan3@gmail.com>
2018-11-04 21:22:44 -0500

    screenshots

--- src/components/pages/Admin.js
+++ src/components/pages/Admin.js
@@ -12,19 +12,15 @@ export default class Admin extends React.Component {
 			<div className = "box cal">
 				<h3>Admin</h3>
 				<div className = "box" id = "Help">
-					<h4>User Manual</h4>
+          <h4>Default Values</h4>
 					<hr/>
-					<b>New Boarding Booking (Existing Client):</b> <span>Search Client or Dog Name <b>>></b> Click New Reservation on Animal Details</span>
+          <b>DaycareRate:</b> <input type = "text" onChange = {this.handleChange} onFocus = {this.handleChange}/>
 					<hr/>
-					<b>New Boarding Booking (New Client):</b> <span>Add the client from New Client page <b>>></b> Rest is the same with the existing client</span>
+          <b>BoardingRate:</b> <input type = "text" onChange = {this.handleChange} onFocus = {this.handleChange}/>
 					<hr/>
-					<b>New Daycare Booking (Existing Client):</b> <span>Search Client or Dog Name <b>>></b> Click New Daycare on Animal Details</span>
+          <b>Discount:</b> <input type = "text" onChange = {this.handleChange} onFocus = {this.handleChange}/>
+          
 					<hr/>
-					<b>New Daycare Booking (New Client):</b> <span>Add the client from New Client page <b>>></b> Rest is the same with the existing client</span>
-					<hr/>
-					<b>Multiple Boarding Booking:</b> <span>Search Client or Dog Name <b>>></b> Hold and drag the mouse until desired dogs are selected <b>>></b> Click New Reservation on Animal Details</span>
-					<hr/>
-					<b>Managing Multiple Boarding Bookings:</b> <span>Click an animal on the dropdown list <b>>></b> Edit booking details <b>>></b> Click Submit <b>(</b>Submit All will submit all of the animals at once<b>)</b></span>
 				</div>
 			</div>
 		)
