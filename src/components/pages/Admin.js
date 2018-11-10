b17e22a6c2e69658a56d6fe96ec9c97dbbb195fb
Omer Dogan <omerdogan3@gmail.com>
2018-11-10 10:40:37 -0500

    tried to keep default values in a file

--- src/components/pages/Admin.js
+++ src/components/pages/Admin.js
@@ -1,27 +1,69 @@
 'use babel';
 
 import React, { Component } from 'react';
+import DefaultValues from '../../defaultValues';
 
 export default class Admin extends React.Component {
 	constructor(props) {
 		super(props)
+		this.state = {
+			daycareRate: DefaultValues.DayCareRate,
+			boardingRate: DefaultValues.BoardingRate,
+			discount: DefaultValues.Discount
+		};
+
+		// this.handleDiscountChange = this.handleDiscountChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
 	}
+
+	componentDidMount(){
+		console.log(DefaultValues)
+	}
+
+	handleChange(event) {
+		if(event.target.name == "DaycareRate"){
+	    this.setState({daycareRate: event.target.value});
+		}else if(event.target.name == "BoardingRate"){
+	    this.setState({boardingRate: event.target.value});
+		}else if(event.target.name == "Discount"){
+	    this.setState({discount: event.target.value});
+		}
+	}
+
+	handleSubmit(event) {
+		// DefaultValues = {
+		// 	DayCareRate: this.state.daycareRate,
+		// 	BoardingRate: this.state.boardingRate,
+		// 	Discount: this.state.discount
+		// }
+	}
+	
+	// handleDiscountChange(event){
+	// 	console.log(event.target.value)
+	// 	this.setState({discount: event.target.value});
+
+	// }
 
 	render(){
 		return (
 			<div className = "box cal">
 				<h3>Admin</h3>
+				<form onSubmit={this.handleSubmit}>
+
 				<div className = "box" id = "Help">
           <h4>Default Values</h4>
 					<hr/>
-          <b>DaycareRate:</b> <input type = "text" onChange = {this.handleChange} onFocus = {this.handleChange}/>
+          <b>DaycareRate:</b> <input type = "text" name="DaycareRate" value={this.state.daycareRate} onChange = {this.handleChange.bind(this)}/>
 					<hr/>
-          <b>BoardingRate:</b> <input type = "text" onChange = {this.handleChange} onFocus = {this.handleChange}/>
+          <b>BoardingRate:</b> <input type = "text" name="BoardingRate" value={this.state.boardingRate} onChange = {this.handleChange.bind(this)}/>
 					<hr/>
-          <b>Discount:</b> <input type = "text" onChange = {this.handleChange} onFocus = {this.handleChange}/>
+          <b>Discount:</b> <input type = "text" name="Discount" value={this.state.discount} onChange = {this.handleChange.bind(this)}/>
           
 					<hr/>
 				</div>
+					<input type="submit" value="Submit" />
+				</form>
 			</div>
 		)
 	}
---
+++ src/defaultValues.js
@@ -0,0 +1,7 @@
+let defaultValues = {
+  DayCareRate: 21.99,
+  Discount: 0,
+  BoardingRate: 35
+}
+
+export default defaultValues;
